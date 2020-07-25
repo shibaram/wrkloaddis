14457bbfd011997d2dd630ece6c62386791ea6de
shibaram <mspsahoo@gmail.com>
2020-07-25 15:42:42 -0500

    cross origin fix

--- workloadbalance/src/main/java/com/wawa/foodordermanagement/rest/WordOrderController.java
+++ workloadbalance/src/main/java/com/wawa/foodordermanagement/rest/WordOrderController.java
@@ -3,6 +3,7 @@ package com.wawa.foodordermanagement.rest;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,6 +16,7 @@ import com.wawa.foodordermanagement.entity.Order;
 
 @RestController
 @RequestMapping("/order")
+@CrossOrigin(origins = "http://localhost:3000")
 public class WordOrderController {
 
 	@Autowired
